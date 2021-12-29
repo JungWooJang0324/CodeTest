7689eeba456c4b2f6c53c658070b29cd33146953
JungWooJang0324 <chiang030204@gmail.com>
2021-12-29 20:41:22 +0900

    baekJoon

---
+++ baekJoon/11654.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        int txt=sc.next().charAt(0);
+        System.out.println(txt);
+     /*
+     public class Main {
+    	public static void main(String[] args) throws Exception {
+ 
+		int a = System.in.read();
+		System.out.print(a);
+ 
+	}
+}
+     */   
+    }
+    
+}
